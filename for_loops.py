51a2185eda71afc8dd0f1e9270629e9f19241191
mutemip <paulmutemi2030@gmail.com>
2023-03-10 21:00:51 +0300

    file: for_loop

---
+++ for_loops.py
@@ -0,0 +1,22 @@
+# # loops - for and while loops
+
+
+
+# instructors = ["Mutemi", "Skinny", "eva"]
+
+
+# #print(len(instructors))
+
+# # print(instructors[0])
+# # print(instructors[1])
+# # print(instructors[2])
+
+# for instructor in instructors:
+#     print(instructor)
+
+
+
+
+# # using range
+# print(list(range(1, 11)))
+
